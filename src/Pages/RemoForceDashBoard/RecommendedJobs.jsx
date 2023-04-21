ffc4baf900d854f4366043a7c3c12976aa041ac8
Aniketsomkuwar <aniketsomkuwar1101@gmail.com>
2023-04-21 10:48:08 +0530

    changes card all again

--- src/Pages/RemoForceDashBoard/RecommendedJobs.jsx
+++ src/Pages/RemoForceDashBoard/RecommendedJobs.jsx
@@ -3,7 +3,6 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react/button-has-type */
 import React from 'react';
-import { AiOutlineFileDone } from 'react-icons/ai';
 import { BsArrowRight } from 'react-icons/bs';
 import { useMediaQuery } from 'react-responsive';
 // Import Swiper React components
@@ -19,6 +18,7 @@ import { Navigation } from "swiper";
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import JobCard from './RemoforceJobs/JobCategoryPages/JobCardComponent/JobCard';
 
 const RecommendedJobs = () => {
     const { data: recommendedAllJobs } = useQuery(['items'], () =>
@@ -163,93 +163,10 @@ const RecommendedJobs = () => {
                             slidesPerView={slidesToShow}
                             modules={[Navigation]}
                             className="mySwiper">
-                            {recommendedAllJobs?.slice(0,6)?.map((item) => (
+                            {recommendedAllJobs?.slice(0, 6)?.map((item) => (
 
                                 <SwiperSlide key={Math.random()}>
-                                    <div
-
-                                        className=" p-3 m-3 border xl:m-1 shadow-md shadow-[#e5f4ff] bg-white border-[#a5dbff9d] rounded-md"
-                                    >
-                                        <div className="flex gap-3 items-center">
-                                            {item?.startupsProfilePhoto ? (
-                                                <img
-                                                    className="w-8 h-8 rounded-full"
-                                                    src={item.startupsProfilePhoto}
-                                                    alt=""
-                                                />
-                                            ) : (
-                                                <p className="w-8 h-8 grid place-items-center  text-white rounded-full bg-black">
-                                                    {item?.startupsName?.charAt(0).toUpperCase()}
-                                                </p>
-                                            )}
-                                            <h2 className="text-[20px] font-semibold">{item?.title}</h2>
-                                            {/* three dot option button */}
-                                            {/* <div className="dropdown dropdown-end">
-                                                <button type='button'>
-                                                    <label tabIndex={0} className=" px-1 py-1 rounded-md cursor-pointer btn-ghost m-1 text-xs">●●●</label>
-                                                </button>
-                                                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                                                    <li><button type='button'
-                                                        className='font-medium text-sm'
-                                                    >View Applicants</button></li>
-                                                    <li><button
-                                                        className='font-medium text-sm'
-                                                        type='button'>Hide Post</button></li>
-
-                                                    <li>
-                                                        <Link
-                                                        className='font-medium text-sm'
-                                                    >Details
-                                                    </Link>
-                                                    </li>
-                                                </ul>
-                                            </div> */}
-
-                                        </div>
-                                        {/* description, skills */}
-                                        <div className="mt-1">
-                                            <p className="text-start ">
-                                                {item?.description.length > 65
-                                                    ? `${item?.description?.slice(0, 65)}...`
-                                                    : item?.description}
-                                            </p>
-                                            <div className="flex mt-[5px] flex-wrap  w-full lg:gap-1 xl:gap-2">
-                                                {item &&
-                                                    item?.skills?.slice(0,3)?.map((skill) => (
-                                                        <div key={Math.random()} className="bg-[#F0F9FF]  rounded-md ">
-                                                            <p className="text-xs px-[5px] py-[5px] ">
-                                                                {skill}
-                                                            </p>
-                                                        </div>
-                                                    ))}
-                                            </div>
-                                            {/* salary  */}
-                                            <div className="mt-3 border-b pb-2">
-                                                <h4 className="text-start font-bold">
-                                                    Salary : {item?.salary} ₳{' '}
-                                                </h4>
-                                            </div>
-                                            {/* applied and apply now button */}
-                                            <div className="mt-4 flex justify-between">
-                                                <button
-                                                    type="button"
-                                                    className="flex items-center font-semibold px-4 rounded-full py-2  gap-2 bg-[#19A5FF80]"
-                                                >
-                                                    {item?.applied}{' '}
-                                                    <span>
-                                                        <AiOutlineFileDone className="text-xl" />
-                                                    </span>
-                                                </button>
-                                                <button
-                                                    onClick={() => handleApplyNow(item)}
-                                                    className="px-6 py-3 lg:px-3 border border-[#00C42B] lg:py-2 bg-[#7DEC96] text-black text-[13px] font-semibold rounded-md"
-                                                    type="button"
-                                                >
-                                                    Apply Now
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <JobCard key={Math.random()} items={item} applyNowBtn={handleApplyNow} />
                                 </SwiperSlide>
                             ))}
                         </Swiper>
